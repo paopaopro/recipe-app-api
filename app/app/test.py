631a7f4449d0bcb0d51768913ab44640cd583c63
Paola Bustos <paopaopro1808@gmail.com>
2025-12-28 18:25:30 -0600

    Added GitHub Actions

---
+++ app/app/test.py
@@ -0,0 +1,16 @@
+"""
+sample test 
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    #test calc module
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
